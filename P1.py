8a9b4d290494e91010888b4d2b1e5400e5ce842d
lnreyes <lnreyes@ualberta.ca>
2016-03-15 12:41:23 -0600

    More edits

--- P1.py
+++ P1.py
@@ -10,39 +10,48 @@ def main():
 
     setup_oracle_connection()
     
-    #Get user to pick an option
-    print("--------------------------------------------")
-    print("Enter '1' to register a vehicle")
-    print("Enter '2' to make an auto transaction")
-    print("Enter '3' for register a license")
-    print("Enter '4' to record a violation ticket")
-    print("Enter '5' to search")
-    print("Enter 'q' to exit program")
-    print("--------------------------------------------")
-    option = input('Please pick an option: ')
-    if 'q' in option.lower():
-        exit()
-    else:
-        try:
-            option = int(choice)
-        except ValueError as ve:
-            pass
+    while True:
+        #Get user to pick an option
+        print("--------------------------------------------")
+        print("Enter '1' to register a vehicle")
+        print("Enter '2' to make an auto transaction")
+        print("Enter '3' for register a license")
+        print("Enter '4' to record a violation ticket")
+        print("Enter '5' to search")
+        print("--------------------------------------------")
+        option = input('Please pick an option: ')
+        if 'q' in option.lower():
+            exit()
+        else:
+            try:
+                option = int(choice)
+            except ValueError as ve:
+                pass
 
-    if option == 1:
-        #TO DO
-        register_vehicle()
-    elif option == 2:
-        #TO DO
-        transaction()
-    elif option == 3:
-        #TO DO
-        register_license()
-    elif option == 4:
-        #TO DO
-        record_violation()
-    elif option == 5:
-        #TO DO
-        search()
+        if option == 1:
+            #TO DO
+            register_vehicle()
+        elif option == 2:
+            #TO DO
+            transaction()
+        elif option == 3:
+            #TO DO
+            register_license()
+        elif option == 4:
+            #TO DO
+            record_violation()
+        elif option == 5:
+            #TO DO
+            search()
+        else:
+            print("--------------------------------------------")
+            print("Enter '1' to register a vehicle")
+            print("Enter '2' to make an auto transaction")
+            print("Enter '3' for register a license")
+            print("Enter '4' to record a violation ticket")
+            print("Enter '5' to search")
+            print("--------------------------------------------")
+            print('Invalid option! Please pick again.')
 
     return
 
@@ -84,24 +93,139 @@ def register_vehicle():
     # Connect to database
     try:
         con = cx_Oracle.connect(CONNECT_INFO)
-            curs = con.cursor()
-        except cx_Oracle.DatabaseError as exception:
-            error = exception.args
-            print(sys.stderr, "Oracle code: ", error.code)
-            print(sys.stderr, "Oracle message: ", error.message)
-            return
+        curs = con.cursor()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+    #-----------------
+    valid = False
+    while not valid:
+        # Get serial_no of car from user
+        while True:
+            serial_no = input('Please enter the serial number of the vehicle: ').strip()
+            if serial_no == 'q':
+                exit()
+            elif int(serial_no):
+                # Look up serial number if it's already registered
+                try:
+                    curs.execute("SELECT serial_no FROM vehicle WHERE serial_no = '{0}'".format(serial_no))
+                    result.curs.fetchall()
+                
+                    
+
+    
+    #-----------------
+    try:
+        curs.close()
+        con.close()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+
+    #return something
 
 def transaction():
     #TO DO
+    # Connect to database
+    try:
+        con = cx_Oracle.connect(CONNECT_INFO)
+        curs = con.cursor()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+    #-----------------
+    #TO DO
+    #-----------------
+    try:
+        curs.close()
+        con.close()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+
+    #return something
 
 def register_license():
     #TO DO
+    # Connect to database
+    try:
+        con = cx_Oracle.connect(CONNECT_INFO)
+        curs = con.cursor()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+    #-----------------
+    #TO DO
+    #-----------------
+    try:
+        curs.close()
+        con.close()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+
+    #return something
 
 def record_violation():
     #TO DO
+    # Connect to database
+    try:
+        con = cx_Oracle.connect(CONNECT_INFO)
+        curs = con.cursor()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+    #-----------------
+    #TO DO
+    #-----------------
+    try:
+        curs.close()
+        con.close()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+
+    #return something
 
 def search():
+    # Connect to database
+    try:
+        con = cx_Oracle.connect(CONNECT_INFO)
+        curs = con.cursor()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+    #-----------------
     #TO DO
+    #-----------------
+    try:
+        curs.close()
+        con.close()
+    except cx_Oracle.DatabaseError as exception:
+        error = exception.args
+        print(sys.stderr, "Oracle code: ", error.code)
+        print(sys.stderr, "Oracle message: ", error.message)
+        return
+
+    #return something
 
 if __name__ == "__main__":
     main()
